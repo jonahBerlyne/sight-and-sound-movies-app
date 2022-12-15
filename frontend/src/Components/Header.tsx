bf7fc0f37d7592b9b20c1f3e35e0996252cef64d
jonahBerlyne <jonshaber@gmail.com>
2022-12-15 17:23:50 -0500

    created nav items component

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -2,41 +2,41 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from './Logo';
 import { motion, AnimatePresence } from 'framer-motion';
-import { Avatar } from '@mui/material';
 import MenuButton from './MenuButton';
+import NavItems from './NavItems';
 
 function Header() {
   
   const [menuOpen, setMenuOpen] = useState(false);
   const [auth, setAuth] = useState(true);
-
+  
   return (
-    <header className='relative flex flex-row justify-between items-center bg-[#1A1A1B] text-white p-2 mb-0 h-auto overflow-x-hidden font-open'>
-     <div className="container px-4 ml-3 lg:ml-12 flex flex-wrap items-center justify-between">
-       <Link to='/' className='mr-4 py-2 whitespace-nowrap'>
-        <Logo />
-       </Link>
-       <button
-            className='text-primary-100 cursor-pointer text-xl leading-none px-3 mt-1 border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none'
-            type='button'
-            onClick={() => setMenuOpen(!menuOpen)}
+    <header className='flex justify-between items-center py-2 bg-[#1A1A1B]'>
+      <Link to='/' className='ml-10 lg:ml-20 whitespace-nowrap z-50'>
+         <Logo />
+      </Link>
+      <NavItems 
+        auth={auth}
+        menuOpen={menuOpen}
+      />
+      <div className="flex justify-end items-center mt-1 mr-10 lg:hidden">
+        <button
+          className='z-50'
+          type='button'
+          onClick={() => setMenuOpen(!menuOpen)}
         >
          <MenuButton 
-          isOpen={menuOpen}
-          color="#FFFFFF"
-          strokeWidth={1}
+           isOpen={menuOpen}
+           color="#FFFFFF"
+           strokeWidth={1}
          />
         </button>
-     </div>
-     <div
-      className={
-       `text-center hidden md:flex md:gap-x-3 md:mr-3 lg:mr-12 flex-grow items-center justify-center py-1`}
-     >
-      <button className='w-20 sm:text-lg lg:text-xl font-semibold'>Login</button>
-      <button className='w-20 sm:text-lg lg:text-xl font-semibold'>Sign Up</button>
-     </div>
+      </div>
     </header>
   );
 }
 
 export default Header;
+
+
+
